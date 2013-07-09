9c240c41d1b24e76c3ec4f385d105dd98cfb3ce4
Mike Bild <mike@mikebild.com>
2013-07-09 16:05:32 +0200

    reverse polish notation (without operator precedences) added

--- Calculator/Lib/lib/calc.js
+++ Calculator/Lib/lib/calc.js
@@ -8,7 +8,7 @@ var ES = require('./es').ES;
 		return fsm.when({
 			$init: {
 				expression: "",
-				result:""
+				result:"",
 			},
 			enteredNumber: function(state, event){
 				state.expression += event.number + " ";
@@ -25,6 +25,65 @@ var ES = require('./es').ES;
 			reseted: function(state, event){
 				state.expression = "";
 				state.result = "";
+				return state;
+			}
+		});
+	};
+	exports.evaluateRPN = function() {
+		var expressionToRPN = function(state, event){
+			var exp = state.expression.trim().split(" ");
+			exp.forEach(function(value){
+				if(!isNaN(value)){
+					state.rpn.push(value);
+				}
+				if(value === "+" || value === "-"){
+					var op = state.operations.pop();
+					if(op) state.rpn.push(op);
+					state.operations.push(value);
+				}
+			});
+			state.rpn.push(state.operations.pop());
+			return state;
+		};
+
+		var evaluateRPN = function(state, event){
+			while(state.rpn.length > 1){
+				var l = parseInt(state.rpn.shift());
+				var r = parseInt(state.rpn.shift());
+				var o = state.rpn.shift();
+				switch(o){
+					case "+": state.rpn.unshift(l+r); break;
+					case "-": state.rpn.unshift(l-r); break;
+				}
+			}
+			state.result = state.rpn;
+		};
+
+		return fsm.when({
+			$init: {
+				expression: "",
+				rpn: [],
+				operations:[],
+				result: 0,
+			},
+			enteredNumber: function(state, event){
+				state.expression += event.number + " ";
+				return state;
+			},
+			enteredOperation: function(state, event){
+				state.expression += event.operation + " ";
+				return state;
+			},
+			executed: function(state, event){
+				expressionToRPN(state, event);
+				evaluateRPN(state, event);
+				return state;
+			},
+			reseted: function(state, event){
+				state.expression = "";
+				state.rpn = [];
+				state.operations = [];
+				state.result = 0;
 				return state;
 			}
 		});
--- Calculator/Lib/tests/spec.js
+++ Calculator/Lib/tests/spec.js
@@ -1,7 +1,7 @@
 var assert = require("assert"),
 	Calc = require("../lib/calc").Calc;
 	
-describe('Calculator', function(){
+describe('Eval Calculator', function(){
 	describe('evaluate 3+4', function(){
 		it('should return 7', function(){
 			Calc.fsm.emit({type:"reseted", data: {}});
@@ -26,6 +26,35 @@ describe('Calculator', function(){
 			Calc.fsm.emit({type:"executed", data: {}});
 			var actual = Calc.evaluate();
 			assert.equal(actual.result, 11);
+		});
+	});
+});
+
+describe('RPN Calculator (without precedences)', function(){
+	describe('evaluate 3+4', function(){
+		it('should return 7', function(){
+			Calc.fsm.emit({type:"reseted", data: {}});
+			
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
+			Calc.fsm.emit({type:"executed", data: {}});
+			var actual = Calc.evaluateRPN();
+			assert.equal(actual.result, 7);
+		});
+	});
+	describe('evaluate 3+4-2', function(){
+		it('should return 5', function(){
+			Calc.fsm.emit({type:"reseted", data: {}});
+
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"-"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
+			Calc.fsm.emit({type:"executed", data: {}});
+			var actual = Calc.evaluateRPN();
+			assert.equal(actual.result, 5);
 		});
 	});
 });
