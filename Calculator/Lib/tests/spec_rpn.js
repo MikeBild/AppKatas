1bcfbd9a97c215b3980fb4c9de259f0441a7f6ec
Mike Bild <mike@mikebild.com>
2013-07-10 12:00:34 +0200

    tests restructured

--- Calculator/Lib/tests/spec_rpn.js
+++ Calculator/Lib/tests/spec_rpn.js
@@ -1,7 +1,7 @@
 var assert = require("assert"),
 	Calc = require("../src/calc").Calc;
-	
-describe('Eval Calculator', function(){
+
+describe('RPN Calculator (without precedences)', function(){
 	describe('evaluate 3+4', function(){
 		it('should return 7', function(){
 			Calc.fsm.emit({type:"reseted", data: {}});
@@ -10,40 +10,38 @@ describe('Eval Calculator', function(){
 			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
 			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
 			Calc.fsm.emit({type:"executed", data: {}});
-			var actual = Calc.evaluate();
+			var actual = Calc.evaluateRPN();
 			assert.equal(actual.result, 7);
 		});
 	});
-	describe('evaluate 3+4*2', function(){
-		it('should return 11', function(){
+	describe('evaluate 3+4-2', function(){
+		it('should return 5', function(){
 			Calc.fsm.emit({type:"reseted", data: {}});
 
 			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
 			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
-			Calc.fsm.emit({type:"enteredOperation", data: {operation:"*"}});
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
-			Calc.fsm.emit({type:"executed", data: {}});
-			var actual = Calc.evaluate();
-			assert.equal(actual.result, 11);
-		});
-	});
-	describe('evaluate 3+4*2/4-3', function(){
-		it('should return 2', function(){
-			Calc.fsm.emit({type:"reseted", data: {}});
-
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
-			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
-			Calc.fsm.emit({type:"enteredOperation", data: {operation:"*"}});
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
-			Calc.fsm.emit({type:"enteredOperation", data: {operation:"/"}});
 			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
 			Calc.fsm.emit({type:"enteredOperation", data: {operation:"-"}});
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
 			Calc.fsm.emit({type:"executed", data: {}});
-			var actual = Calc.evaluate();
-			assert.equal(actual.result, 2);
+			var actual = Calc.evaluateRPN();
+			assert.equal(actual.result, 5);
+		});
+	});
+	describe('evaluate 3+4-2+5', function(){
+		it('should return 10', function(){
+			Calc.fsm.emit({type:"reseted", data: {}});
+
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"-"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"5"}});
+			Calc.fsm.emit({type:"executed", data: {}});
+			var actual = Calc.evaluateRPN();
+			assert.equal(actual.result, 10);
 		});
 	});
 });
--- Calculator/Lib/tests/spec_rpn_op.js
+++ Calculator/Lib/tests/spec_rpn_op.js
@@ -1,19 +1,7 @@
 var assert = require("assert"),
 	Calc = require("../src/calc").Calc;
-	
-describe('Eval Calculator', function(){
-	describe('evaluate 3+4', function(){
-		it('should return 7', function(){
-			Calc.fsm.emit({type:"reseted", data: {}});
-			
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
-			Calc.fsm.emit({type:"enteredOperation", data: {operation:"+"}});
-			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
-			Calc.fsm.emit({type:"executed", data: {}});
-			var actual = Calc.evaluate();
-			assert.equal(actual.result, 7);
-		});
-	});
+
+describe('RPN Calculator (with precedences)', function(){
 	describe('evaluate 3+4*2', function(){
 		it('should return 11', function(){
 			Calc.fsm.emit({type:"reseted", data: {}});
@@ -24,8 +12,22 @@ describe('Eval Calculator', function(){
 			Calc.fsm.emit({type:"enteredOperation", data: {operation:"*"}});
 			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
 			Calc.fsm.emit({type:"executed", data: {}});
-			var actual = Calc.evaluate();
+			var actual = Calc.evaluateRPNOP();
 			assert.equal(actual.result, 11);
+		});
+	});
+	describe('evaluate 3*4-2', function(){
+		it('should return 10', function(){
+			Calc.fsm.emit({type:"reseted", data: {}});
+
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"*"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"4"}});
+			Calc.fsm.emit({type:"enteredOperation", data: {operation:"-"}});
+			Calc.fsm.emit({type:"enteredNumber", data: {number:"2"}});
+			Calc.fsm.emit({type:"executed", data: {}});
+			var actual = Calc.evaluateRPNOP();
+			assert.equal(actual.result, 10);
 		});
 	});
 	describe('evaluate 3+4*2/4-3', function(){
@@ -42,7 +44,7 @@ describe('Eval Calculator', function(){
 			Calc.fsm.emit({type:"enteredOperation", data: {operation:"-"}});
 			Calc.fsm.emit({type:"enteredNumber", data: {number:"3"}});
 			Calc.fsm.emit({type:"executed", data: {}});
-			var actual = Calc.evaluate();
+			var actual = Calc.evaluateRPNOP();
 			assert.equal(actual.result, 2);
 		});
 	});
